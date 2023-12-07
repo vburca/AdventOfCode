46f9fc2194ce545f62094a399f4ae29255b25749
Vlad Burca <vladburca2005@gmail.com>
2023-12-06 22:43:26 -0800

    Add day7 2023

---
+++ 2023/day07/CamelCards.cpp
@@ -0,0 +1,257 @@
+#include "../../util/StringUtil.h"
+#include "../../util/NumbersUtil.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cstdint>
+#include <vector>
+#include <regex>
+#include <unordered_map>
+#include <array>
+#include <set>
+#include <utility>
+
+using namespace std;
+
+regex HAND_RX("([AKQJT2-9]+) ([0-9]+)");
+
+const size_t kDeckSize = 13;
+const char kJokerCard = 'J';
+
+const vector<char> cardLabels = {
+    '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
+};
+
+const map<char, size_t> cardRanking = {
+    {'2', 0},
+    {'3', 1},
+    {'4', 2},
+    {'5', 3},
+    {'6', 4},
+    {'7', 5},
+    {'8', 6},
+    {'9', 7},
+    {'T', 8},
+    {'J', 9},
+    {'Q', 10},
+    {'K', 11},
+    {'A', 12},
+};
+
+const map<char, size_t> cardRankingJoker = {
+    {'J', 0},
+    {'2', 1},
+    {'3', 2},
+    {'4', 3},
+    {'5', 4},
+    {'6', 5},
+    {'7', 6},
+    {'8', 7},
+    {'9', 8},
+    {'T', 9},
+    {'Q', 10},
+    {'K', 11},
+    {'A', 12},
+};
+
+enum HandType {
+    None = 0,
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfKind,
+    FullHouse,
+    FourOfKind,
+    FiveOfKind,
+};
+
+struct Hand {
+    string cards;
+    size_t bid;
+};
+
+struct HandComparator {
+    bool operator() (const Hand &lhs, const Hand &rhs) const {
+        for (size_t i = 0; i < lhs.cards.size(); i++) {
+            const auto lCard = lhs.cards[i];
+            const auto rCard = rhs.cards[i];
+
+            const auto lRanking = cardRanking.at(lCard);
+            const auto rRanking = cardRanking.at(rCard);
+            if (lRanking == rRanking) {
+                continue;
+            }
+
+            return lRanking < rRanking;
+        }
+
+        return true;
+    }
+};
+
+struct HandComparatorJoker {
+    bool operator() (const Hand &lhs, const Hand &rhs) const {
+        for (size_t i = 0; i < lhs.cards.size(); i++) {
+            const auto lCard = lhs.cards[i];
+            const auto rCard = rhs.cards[i];
+
+            const auto lRanking = cardRankingJoker.at(lCard);
+            const auto rRanking = cardRankingJoker.at(rCard);
+            if (lRanking == rRanking) {
+                continue;
+            }
+
+            return lRanking < rRanking;
+        }
+
+        return true;
+    }
+};
+
+vector<Hand> _parseInput(istream& inputFile) {
+    vector<Hand> hands;
+
+    while (!inputFile.eof()) {
+        string line;
+        getline(inputFile, line);
+
+        smatch handMatch;
+        if (!regex_match(line, handMatch, HAND_RX)) {
+            cout << "HAND_RX failed to match line: { " << line << " }" << endl;
+            exit(1);
+        }
+
+        hands.push_back({
+            .cards = handMatch[1].str(),
+            .bid = static_cast<size_t>(stoul(handMatch[2].str())),
+        });
+    }
+
+    return hands;
+}
+
+HandType _computeHandType(const Hand &hand, optional<char> jokerReplacement = nullopt) {
+    array<size_t, kDeckSize> cardCount { 0 };
+
+    for (const auto c : hand.cards) {
+        char card = c;
+        if (jokerReplacement && c == kJokerCard) {
+            card = *jokerReplacement;
+        }
+        cardCount[cardRanking.at(card)] += 1;
+    }
+
+    size_t threeCount = 0;
+    size_t twoCount = 0;
+    for (const auto count : cardCount) {
+        if (count == 5) {
+            return FiveOfKind;
+        } else if (count == 4) {
+            return FourOfKind;
+        } else if (count == 3) {
+            threeCount += 1;
+        } else if (count == 2) {
+            twoCount += 1;
+        }
+    }
+
+    if (threeCount == 1 && twoCount == 1) {
+        return FullHouse;
+    } else if (threeCount == 1) {
+        return ThreeOfKind;
+    }
+
+    if (twoCount == 2) {
+        return TwoPair;
+    }
+
+    if (twoCount == 1) {
+        return OnePair;
+    }
+
+    return HighCard;
+}
+
+void part1(const vector<Hand> &hands) {
+    map<HandType, set<Hand, HandComparator>> typeToHands;
+
+    for (const auto &hand : hands) {
+        const auto type = _computeHandType(hand);
+        typeToHands[type].insert(hand);
+    }
+
+    int64_t totalWinnings = 0;
+    size_t rank = 1;
+    for (const auto &p : typeToHands) {
+        for (const auto &hand : p.second) {
+            totalWinnings += rank * hand.bid;
+            rank += 1;
+        }
+    }
+
+    cout << totalWinnings << endl;
+}
+
+void part2(const vector<Hand> &hands) {
+    map<HandType, set<Hand, HandComparatorJoker>> typeToHands;
+
+    for (const auto &hand : hands) {
+        HandType strongestType = _computeHandType(hand);
+        if (hand.cards.contains(kJokerCard) && strongestType < FiveOfKind) {
+            for (const auto card : cardLabels) {
+                if (card == kJokerCard) {
+                    continue;
+                }
+
+                Hand shapeshiftHand = hand;
+                replace(shapeshiftHand.cards.begin(), shapeshiftHand.cards.end(), kJokerCard, card);
+
+                const auto type = _computeHandType(shapeshiftHand);
+                if (type > strongestType) {
+                    strongestType = type;
+                    if (strongestType == FiveOfKind) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        typeToHands[strongestType].insert(hand);
+    }
+
+    int64_t totalWinnings = 0;
+    size_t rank = 1;
+    for (const auto &p : typeToHands) {
+        for (const auto &hand : p.second) {
+            totalWinnings += rank * hand.bid;
+            rank += 1;
+        }
+    }
+
+    cout << totalWinnings << endl;
+}
+
+
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        std::cout << "Must specify input file!" << endl;
+        return 0;
+    }
+
+    std::cout << "Input file name: " << argv[1] << endl;
+    ifstream inputFile(argv[1]);
+
+    const auto hands = _parseInput(inputFile);
+
+    std::cout << "Part 1: " << endl;
+    part1(hands);
+
+    std::cout << "Part 2: " << endl;
+    part2(hands);
+
+    inputFile.close();
+
+    return 0;
+}
