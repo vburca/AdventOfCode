154681179ee299eb9e0a8035b8956d8a1f7a4e6b
Vlad Burca <vladburca2005@gmail.com>
2023-12-17 17:59:24 +0200

    Add types util

---
+++ util/Types.h
@@ -0,0 +1,11 @@
+#include <utility>
+
+namespace aoc {
+namespace util {
+namespace types {
+
+typedef std::pair<size_t, size_t> coord_t;
+
+}  // namespace types
+}  // namespace util
+}  // namespace aoc
