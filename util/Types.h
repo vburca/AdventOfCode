68c6c1961e61c9f80c0a82d4c115d0fba0105f44
Vlad Burca <vladburca2005@gmail.com>
2023-12-23 13:20:44 +0200

    Add 3d coordinate type

--- util/Types.h
+++ util/Types.h
@@ -7,6 +7,34 @@ namespace types {
 typedef std::pair<size_t, size_t> coord_t;
 typedef std::pair<int64_t, int64_t> coord_signed_t;
 
+struct coord3d_t {
+    size_t x;
+    size_t y;
+    size_t z;
+
+    bool operator==(const coord3d_t &other) const {
+        return std::tie(x, y, z) == std::tie(other.x, other.y, other.z);
+    }
+
+    bool operator<(const coord3d_t &other) const {
+        return std::tie(x, y, z) < std::tie(other.x, other.y, other.z);
+    }
+};
+
+struct coord3d_signed_t {
+    int64_t x;
+    int64_t y;
+    int64_t z;
+
+    bool operator==(const coord3d_signed_t &other) const {
+        return std::tie(x, y, z) == std::tie(other.x, other.y, other.z);
+    }
+
+    bool operator<(const coord3d_signed_t &other) const {
+        return std::tie(x, y, z) < std::tie(other.x, other.y, other.z);
+    }
+};
+
 }  // namespace types
 }  // namespace util
 }  // namespace aoc
