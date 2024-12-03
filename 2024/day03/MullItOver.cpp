3dd8b00567535d5e59c9e9312672cabf10ffa29a
Vlad Burca <vladburca2005@gmail.com>
2024-12-03 13:01:00 -0800

    Fix regex and clean up with OR statements

--- 2024/day03/MullItOver.cpp
+++ 2024/day03/MullItOver.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 
 const regex MUL_RX("mul\\(([0-9]+),([0-9]+)\\)");
+const regex MUL_RX_AUX("(do\\(\\)|don't\\(\\)|mul\\(([0-9]+),([0-9]+)\\))");
+
 
 void part1(istream& inputFile) {
     uint64_t result = 0;
@@ -42,7 +44,7 @@ void part1(istream& inputFile) {
     cout << result << endl;
 }
 
-void part2(istream& inputFile) {
+void part2_orig(istream& inputFile) {
     uint64_t result = 0;
 
     vector<pair<uint64_t, uint64_t>> mulNumbers;
@@ -76,6 +78,54 @@ void part2(istream& inputFile) {
             if (isEnabled) {
                 stringstream num1Str(match[1].str());
                 stringstream num2Str(match[2].str());
+                uint64_t num1;
+                uint64_t num2;
+                num1Str >> num1;
+                num2Str >> num2;
+
+                // cout << "(" << num1 << ", " << num2 << ")" << endl;
+
+                mulNumbers.emplace_back(make_pair(num1, num2));
+            }
+
+            remainingLine = match.suffix(); // assign the line portion remaining, after the match
+            // cout << endl;
+        }
+    }
+
+    for (const auto &nums : mulNumbers) {
+        result += nums.first * nums.second;
+    }
+
+    cout << result << endl;
+}
+
+void part2(istream& inputFile) {
+    uint64_t result = 0;
+
+    vector<pair<uint64_t, uint64_t>> mulNumbers;
+
+    bool isEnabled = true;
+    smatch match;
+    while (!inputFile.eof()) {
+        string line;
+        getline(inputFile, line);
+
+        string remainingLine = line;
+        while (regex_search(remainingLine, match, MUL_RX_AUX)) {
+            if (match[0] == "do()") {
+                isEnabled = true;
+                remainingLine = match.suffix();
+                continue;
+            } else if (match[0] == "don't()") {
+                isEnabled = false;
+                remainingLine = match.suffix();
+                continue;
+            }
+
+            if (isEnabled) {
+                stringstream num1Str(match[2].str());
+                stringstream num2Str(match[3].str());
                 uint64_t num1;
                 uint64_t num2;
                 num1Str >> num1;
